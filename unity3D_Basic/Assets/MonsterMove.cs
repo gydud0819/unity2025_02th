8f227f2f7688ebc79c32170d0abf1103abb137a1
gydud0819 <pstar0819@naver.com>
2025-08-26 17:49:11 +0900

    (2025-08-26) 유니티 코루틴 실습 및 예제 적용

---
+++ unity3D_Basic/Assets/MonsterMove.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMove : MonoBehaviour
+{
+    // 2d 월드에서 랜덤한 위치로 이동하는 코드를 작성해줘
+    // 이동 속도는 얼마인지, 이동하는 방식은 무엇인지 rigidbody 2D를 이용한 물리엔진 방식
+    // 서로 충돌했을 때 어떻게 되는지 = 서로 충돌하면 사라지게 해보기 
+    // 함수이름 : Move
+    // Start is called before the first frame update
+
+    [Header("몬스터 움직이기")]
+    [SerializeField] float moveSpeed = 3.0f;
+    [SerializeField] Rigidbody2D rigidbody2D;
+
+    private Vector2 monsterPosition;
+
+    void Start()
+    {
+        if (rigidbody2D == null)
+        {
+            rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        RandomPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        if(Vector2.Distance(transform.position, monsterPosition) < 1.0f)
+        {
+            RandomPosition();
+        }
+
+        Vector2 dir = (monsterPosition - (Vector2)transform.position).normalized;
+        rigidbody2D.velocity = dir * moveSpeed;
+    }
+
+    public void RandomPosition()
+    {
+        Camera mainCamera = Camera.main;
+
+        if(mainCamera == null)
+        {
+            return;
+        }
+
+        float randX = mainCamera.transform.position.x;
+        float randY = mainCamera.transform.position.y;
+
+        float screenAspect = (float) Screen.width / (float)Screen.height;   // 해상도는 정수니까 float으로 명시적 형변환 해주기
+        float cameraHeight = mainCamera.orthographicSize * 2f;   // 카메라 화면 넓이 구하는 코드
+        float cameraWidth = cameraHeight * screenAspect;
+
+        float randomX = Random.Range(randX - cameraWidth / 2, randX + cameraWidth / 2);
+        float randomY = Random.Range(randY - cameraWidth / 2, randY + cameraWidth / 2);
+
+        monsterPosition=  new Vector2(randomX, randomY);
+    }
+
+}
---
+++ unity3D_Basic/Assets/MonsterSpawner.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    // 특정 시점, 특정 이벤트가 발생되고 나서 몬스터를 생성하고 싶음.
+
+    [Header("몬스터 생성 정보")]
+    [SerializeField] Transform[] spwanPositions;
+    [SerializeField] GameObject[] monsterSpwaners;
+    [SerializeField] int spwanCount = 5;
+    [SerializeField] float spwanIntervalTime = 0.75f;
+
+    private Coroutine spwanCoroutine;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Spawn();
+        }
+    }
+
+    /// <summary>
+    /// 게임 월드 특정 위치에 몬스터를 생성할 때 몇마리를
+    /// 생성할 것인지, 한번에 등장할 것인지 (그냥 반복문 돌리기), 시간에 걸쳐서 서서히 생성할 것인지 (코루틴 사용)
+    /// ex). 유니티에서 함수 이름이 spwan이고 위의 두줄의 기능을 하는 함수를 만들어줘
+    /// </summary>
+
+    public void Spawn()
+    {
+        if (spwanCoroutine != null)
+        {
+            StopCoroutine(SpwanCoroutine());
+
+        }
+        spwanCoroutine = StartCoroutine(SpwanCoroutine());
+
+        //StartCoroutine("SpwanCoroutine");       // 이 코드의 문제점 : 오타, 어디에서 문제가 발생했는지 버그를 찾기 어려움.
+        //StartCoroutine(nameof(SpwanCoroutine)); // 문자열로 쓰고 싶다면 nameof 사용하기
+
+        // 2가지 방식중에 어떤 코루틴 호출 방식을 사용하면 될까?
+        // 두 방식 중에 원하는 방식을 사용하되, 방식을 통일해서 사용하기 
+
+        // ※ 일관성있게 작성하기
+        //StopCoroutine(SpwanCoroutine());
+        //StopCoroutine(nameof(SpwanCoroutine));
+    }
+
+    private IEnumerator SpwanCoroutine()
+    {
+        for (int i = 0; i < spwanCount; i++)
+        {
+            int randomIndex = UnityEngine.Random.Range(0, spwanPositions.Length);
+            int randomMonIndex = UnityEngine.Random.Range(0, monsterSpwaners.Length);
+            Instantiate(monsterSpwaners[randomMonIndex], spwanPositions[randomIndex]);
+
+            // interval 시간 후에 위의 코드를 다시 실행하라.
+            yield return new WaitForSeconds(spwanIntervalTime);
+        }
+    }
+
+}
